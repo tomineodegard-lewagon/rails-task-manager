efe19442aa13967104afa5193d0c3ae07c02bb57
Tomine Odegard <tomineodegard@hotmail.no>
2020-08-15 15:42:24 +0200

    To do task challenge, a work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/tasks", to: "tasks#index"
+  get "/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "/tasks", to: "tasks#create"
+
+
+#     tasks GET    /tasks(.:format)          tasks#index
+#           POST   /tasks(.:format)          tasks#create
+#  new_task GET    /tasks/new(.:format)      tasks#new
+# edit_task GET    /tasks/:id/edit(.:format) tasks#edit
+#      task GET    /tasks/:id(.:format)      tasks#show
+#           PATCH  /tasks/:id(.:format)      tasks#update
+#           PUT    /tasks/:id(.:format)      tasks#update
+#           DELETE /tasks/:id(.:format)      tasks#destroy
 end
